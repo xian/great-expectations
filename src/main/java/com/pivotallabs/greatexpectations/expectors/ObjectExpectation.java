ef81d4789a5ae33d844deab644d0942f8b5cb037
Christian Williams <xian@squareup.com>
2011-03-21 10:08:04 -0700

    Fix toBe() and toEqual().

--- src/main/java/com/pivotallabs/greatexpectations/expectors/ObjectExpectation.java
+++ src/main/java/com/pivotallabs/greatexpectations/expectors/ObjectExpectation.java
@@ -5,13 +5,13 @@ import com.pivotallabs.greatexpectations.ExpectationOn;
 @ExpectationOn(Object.class)
 public class ObjectExpectation<T, M extends ObjectExpectation<T, M>> extends BaseExpectation<T, M> {
   public boolean toEqual(T expected) {
-    if (actual == null) return false;
+    if (actual == null) throw new NullPointerException("actual is null");
     return actual.equals(expected);
   }
 
   public boolean toBe(T expected) {
-    return false;
-//        match(Matchers.sameInstance(expected));
+    if (actual == null) throw new NullPointerException("actual is null");
+    return actual == expected;
   }
 
   public boolean toBeInstanceOf(Class<? extends T> expected) {
